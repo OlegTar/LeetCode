602a2f9d2bd5c5027757cc599afa2877bf150a6b
Oleg <ot88899888@gmail.com>
2025-09-15 12:12:42 +0300

    Create 374. Guess Number Higher or Lower.cs

---
+++ 374. Guess Number Higher or Lower.cs
@@ -0,0 +1,34 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        var left = 0;
+        var right = n;
+        var mid = 0;
+        while (left <= right)
+        {
+            mid = left + (right - left) / 2;
+            var result = guess(mid);
+            if (result == 0)
+            {                
+                return mid;
+            }
+            else if (result == -1)
+            {
+                right = mid - 1;
+            }
+            else if (result == 1)
+            {
+                left = mid + 1;
+            }
+        }
+        return mid;
+    }
+}
