750057e1e838160015919787680d3f23300fdd88
Oleg <ot88899888@gmail.com>
2025-09-20 04:17:13 +0300

    Update 52. Peak Index in a Mountain Array.cs
    убрано лишнее

--- 52. Peak Index in a Mountain Array.cs
+++ 52. Peak Index in a Mountain Array.cs
@@ -7,14 +7,6 @@ public class Solution {
         {
             var mid = left + (right - left) / 2;
             
-            if (mid == 0 && arr[0] > arr[1]) {
-                return mid;
-            }
-
-            if ((mid == arr.Length - 1) && arr[^2] < arr[mid]) {
-                return mid;
-            }
-
             if ((mid > 0 && mid < (arr.Length - 1)) && arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
             {
                 return mid;
