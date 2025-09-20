4cb0957bc5ffe5ca58f5027c586873bf7cd690d1
Oleg <ot88899888@gmail.com>
2025-09-20 04:10:48 +0300

    Create 52. Peak Index in a Mountain Array.cs
    You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
    
    Return the index of the peak element.
    
    Your task is to solve it in O(log(n)) time complexity.
    
     
    
    Example 1:
    
    Input: arr = [0,1,0]
    
    Output: 1
    
    Example 2:
    
    Input: arr = [0,2,1,0]
    
    Output: 1
    
    Example 3:
    
    Input: arr = [0,10,5,2]
    
    Output: 1

---
+++ 52. Peak Index in a Mountain Array.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int PeakIndexInMountainArray(int[] arr) {
+        var left = 0;
+        var right = arr.Length - 1;
+        
+        while (left < right)
+        {
+            var mid = left + (right - left) / 2;
+            
+            if (mid == 0 && arr[0] > arr[1]) {
+                return mid;
+            }
+
+            if ((mid == arr.Length - 1) && arr[^2] < arr[mid]) {
+                return mid;
+            }
+
+            if ((mid > 0 && mid < (arr.Length - 1)) && arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            {
+                return mid;
+            }
+
+            if (arr[mid] < arr[mid + 1])
+            {
+                left = mid + 1;
+            }
+            else 
+            {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
