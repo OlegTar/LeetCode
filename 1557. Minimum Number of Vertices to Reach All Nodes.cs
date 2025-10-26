dc1be64001468d76ee59955cba29a62cf0184717
Oleg <ot88899888@gmail.com>
2025-10-26 18:16:53 +0300

    Create 1557. Minimum Number of Vertices to Reach All Nodes.cs

---
+++ 1557. Minimum Number of Vertices to Reach All Nodes.cs
@@ -0,0 +1,33 @@
+/*Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
+
+Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique solution exists.
+
+Notice that you can return the vertices in any order.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 6, edges = [[0,1],[0,2],[2,5],[3,4],[4,2]]
+Output: [0,3]
+Explanation: It's not possible to reach all the nodes from a single vertex. From 0 we can reach [0,1,2,5]. From 3 we can reach [3,4,2,5]. So we output [0,3].*/
+public class Solution {
+    public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges) {
+        Span<int> nodes = stackalloc int[n];
+        for (var i = 0; i < edges.Count; i++)
+        {
+            nodes[edges[i][1]] = 1;
+        }
+        var result = new List<int>();
+        for (var i = 0; i < n; i++)
+        {
+            if (nodes[i] == 0) 
+            {
+                result.Add(i);
+            }
+        }
+        return result;        
+    }
+}
