0bb6f511b58cdd3aaef077c6ecafb49514989500
Oleg <ot88899888@gmail.com>
2025-12-22 14:38:49 +0300

    Create 1116. Print Zero Even Odd.cs

---
+++ 1116. Print Zero Even Odd.cs
@@ -0,0 +1,107 @@
+/*
+You have a function printNumber that can be called with an integer parameter and prints it to the console.
+
+For example, calling printNumber(7) prints 7 to the console.
+You are given an instance of the class ZeroEvenOdd that has three functions: zero, even, and odd. The same instance of ZeroEvenOdd will be passed to three different threads:
+
+Thread A: calls zero() that should only output 0's.
+Thread B: calls even() that should only output even numbers.
+Thread C: calls odd() that should only output odd numbers.
+Modify the given class to output the series "010203040506..." where the length of the series must be 2n.
+
+Implement the ZeroEvenOdd class:
+
+ZeroEvenOdd(int n) Initializes the object with the number n that represents the numbers that should be printed.
+void zero(printNumber) Calls printNumber to output one zero.
+void even(printNumber) Calls printNumber to output one even number.
+void odd(printNumber) Calls printNumber to output one odd number.
+ 
+
+Example 1:
+
+Input: n = 2
+Output: "0102"
+Explanation: There are three threads being fired asynchronously.
+One of them calls zero(), the other calls even(), and the last one calls odd().
+"0102" is the correct output.
+Example 2:
+
+Input: n = 5
+Output: "0102030405"
+ 
+
+Constraints:
+
+1 <= n <= 1000
+*/
+using System.Threading;
+
+public class ZeroEvenOdd {    
+    private int n;
+    private int count = 0;
+    private int number = 1;
+    private bool oddNumber = true;
+    
+    private AutoResetEvent zero = new AutoResetEvent(true);
+    private AutoResetEvent odd = new AutoResetEvent(false);
+    private AutoResetEvent even = new AutoResetEvent(false);
+    
+    public ZeroEvenOdd(int n) {
+        this.n = 2 * n;
+    }
+
+    public void Zero(Action<int> printNumber) {        
+        while (true)
+        {
+            zero.WaitOne();            
+            
+            if (count == n)
+            {
+                odd.Set();
+                even.Set();
+                return;
+            }
+
+            printNumber(0);
+            count++;
+
+            if (oddNumber)
+            {                             
+                odd.Set();
+            }
+            else 
+            {
+                even.Set();
+            }
+            oddNumber = !oddNumber;
+        }
+    }
+
+    public void Even(Action<int> printNumber) {        
+        while (true)
+        {            
+            even.WaitOne();
+            if (count == n)
+            {
+                return;
+            }
+            printNumber(number++);
+            count++;
+            zero.Set();
+        }
+    }
+
+    public void Odd(Action<int> printNumber) {        
+        while (true)
+        {
+            odd.WaitOne(); 
+            if (count == n)
+            {
+                return;
+            }
+            printNumber(number++);
+            count++;
+            zero.Set();
+        }
+    }
+}
