edb0c1448d35bc240b5cbcfde59127212e913eed
Oleg <ot88899888@gmail.com>
2026-02-13 14:23:28 +0300

    Create 176. Second Highest Salary.sql

---
+++ 176. Second Highest Salary.sql
@@ -0,0 +1,63 @@
+/*SQL Schema
+Pandas Schema
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+*/
+select Salary "SecondHighestSalary" from (
+    Select Salary, rn From (
+        select Salary, rank() over (order by Salary desc) rn From (
+            select distinct Salary From Employee order by Salary desc
+        )
+        UNION
+        select NULL, 3
+    ) as t order by Salary desc nulls last
+) t
+where rn > 1 and rn <= 3
+limit 1
