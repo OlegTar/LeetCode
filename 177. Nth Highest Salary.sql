587c6fb84a8af2c5062e388b584a2058849f43b2
Oleg <ot88899888@gmail.com>
2026-02-13 15:13:02 +0300

    Create 177. Nth Highest Salary.sql

---
+++ 177. Nth Highest Salary.sql
@@ -0,0 +1,66 @@
+/*Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+
+ 
+*/
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+    Select t.Salary From (
+        select t.Salary, rank() over (order by t.Salary desc) rn from (
+            SELECT DISTINCT t.Salary from Employee t
+        ) as t
+    ) as t where rn = N
+  );
+END;
+$$ LANGUAGE plpgsql;
