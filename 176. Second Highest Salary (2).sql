c04e29343e5cff9599accf7b9bd3baf0e1ffe386
Oleg <ot88899888@gmail.com>
2026-02-13 14:42:13 +0300

    Create 176. Second Highest Salary (2).sql

---
+++ 176. Second Highest Salary (2).sql
@@ -0,0 +1,62 @@
+/*Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+ 
+*/
+with t as (
+select Salary, rank() over (order by Salary desc) rn From (
+    select distinct Salary From Employee order by Salary desc
+) limit 2)
+Select Salary "SecondHighestSalary" from (
+    Select Salary, rn from t where rn = 2
+    union all
+    Select null, 2
+) 
+where rn = 2 limit 1
