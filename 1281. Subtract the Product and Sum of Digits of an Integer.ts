294f892ac5c4853f4c5d4ca9453ad2bf63ef3eb4
Oleg <ot88899888@gmail.com>
2026-03-22 22:22:53 +0300

    Create 1281. Subtract the Product and Sum of Digits of an Integer.ts

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.ts
@@ -0,0 +1,44 @@
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+*/
+function subtractProductAndSum(n: number): number {
+    let product: number = 1;
+    let sum: number = 0;
+
+    for (const digit of getDigits(n)) {    
+        product *= +digit;
+        sum += +digit;
+    }
+
+    return product - sum;
+};
+
+function* getDigits(n: number): Generator<number> {
+    while (n > 0) {
+        yield n % 10;
+        n = Math.floor(n / 10);
+    }
+}
