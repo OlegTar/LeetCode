798183754c1ea5d517316ba593b78f164cbd243f
Oleg <ot88899888@gmail.com>
2026-03-29 00:17:27 +0300

    Create 92. Reverse Linked List II.go

---
+++ 92. Reverse Linked List II.go
@@ -0,0 +1,60 @@
+/*Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+ 
+
+Follow up: Could you do it in one pass?*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+    if (left == right) {
+        return head
+    }
+
+    dummy := ListNode{
+        Val: 0,
+        Next: head,
+    }
+
+    var prev *ListNode = &dummy
+    var node *ListNode = head
+    for i := 0; i < (left - 1); i++ {
+        prev = node
+        node = node.Next
+    }
+
+    diff := right - left
+    for j := 0; j < diff; j++ {
+        firstAfterPrev := prev.Next        
+        next := node.Next
+        nextNext := next.Next
+        prev.Next = next
+        next.Next = firstAfterPrev
+        node.Next = nextNext
+    }
+
+    return dummy.Next
+}
