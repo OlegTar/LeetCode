ccc959ebc09670364e1417cd087ee6edc34e0ccb
Oleg <ot88899888@gmail.com>
2026-04-04 15:06:18 +0300

    Create 108. Convert Sorted Array to Binary Search Tree.cs

---
+++ 108. Convert Sorted Array to Binary Search Tree.cs
@@ -0,0 +1,53 @@
+/*Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+    if (len(nums) == 0) {
+        return nil;
+    }
+
+    if (len(nums) == 1) {
+        return &TreeNode{
+            Val: nums[0],
+        }
+    }
+
+    middle := len(nums) / 2;
+    node := &TreeNode{
+        Val: nums[middle],
+    }
+
+    node.Left = sortedArrayToBST(nums[:middle]);
+    node.Right = sortedArrayToBST(nums[middle + 1:]);
+
+    return node;
+}
