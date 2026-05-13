c194880883dcbbc0b7222e3927d0173109f805a0
Oleg <ot88899888@gmail.com>
2026-05-14 00:35:15 +0300

    Update 876. Middle of the Linked List.cs

--- 876. Middle of the Linked List.cs
+++ 876. Middle of the Linked List.cs
@@ -33,17 +33,28 @@ The number of nodes in the list is in the range [1, 100].
  *     }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode MiddleNode(ListNode head) {
-        var slow = head;
-        var fast = head;
-        while (fast != null && fast.next != null)
+        (var slow, var fast) = (head, head);
+        
+        while (fast?.next != null)
         {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        if (fast != null && fast.next != null) {
+        if (fast?.next != null) {
             slow = slow.next;
         }
 
