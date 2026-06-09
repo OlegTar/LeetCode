ab352d29702c49d4724ffa21738b62a5613c0fc8
Oleg <ot88899888@gmail.com>
2026-06-09 09:39:41 +0300

    Create 814. Binary Tree Pruning.cs

---
+++ 814. Binary Tree Pruning.cs
@@ -0,0 +1,63 @@
+/*
+Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+A subtree of a node node is node plus every node that is a descendant of node.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,0,0,1]
+Output: [1,null,0,null,1]
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+Example 2:
+
+
+Input: root = [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+Example 3:
+
+
+Input: root = [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 200].
+Node.val is either 0 or 1.*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode PruneTree(TreeNode root) {
+        if (root == null)
+        {
+            return null;
+        }
+        
+        root.left = PruneTree(root.left);
+        root.right = PruneTree(root.right);
+        
+        if (root.val == 0
+          && root.left == null
+          && root.right == null
+        ) {
+            return null;
+        }
+        return root;
+    }
+}
