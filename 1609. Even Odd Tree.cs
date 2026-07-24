cf3e25a8d1ae3ae42d0dfbddf9e00087c4228c42
Oleg <ot88899888@gmail.com>
2026-07-24 08:57:55 +0300

    Create 1609. Even Odd Tree.cs

---
+++ 1609. Even Odd Tree.cs
@@ -0,0 +1,107 @@
+/*A binary tree is named Even-Odd if it meets the following conditions:
+
+The root of the binary tree is at level index 0, its children are at level index 1, their children are at level index 2, etc.
+For every even-indexed level, all nodes at the level have odd integer values in strictly increasing order (from left to right).
+For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
+Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,10,4,3,null,7,9,12,8,6,null,null,2]
+Output: true
+Explanation: The node values on each level are:
+Level 0: [1]
+Level 1: [10,4]
+Level 2: [3,7,9]
+Level 3: [12,8,6,2]
+Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even and decreasing, the tree is Even-Odd.
+Example 2:
+
+
+Input: root = [5,4,2,3,3,7]
+Output: false
+Explanation: The node values on each level are:
+Level 0: [5]
+Level 1: [4,2]
+Level 2: [3,3,7]
+Node values in level 2 must be in strictly increasing order, so the tree is not Even-Odd.
+Example 3:
+
+
+Input: root = [5,9,1,3,5,7]
+Output: false
+Explanation: Node values in the level 1 should be even integers.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 106*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsEvenOddTree(TreeNode root) {
+        var even = true;
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var n = queue.Count;
+            var prev = even ? int.MinValue : int.MaxValue;
+            for (var i = 0; i < n; i++)
+            {
+                var node = queue.Dequeue();
+                if (even)
+                {
+                    if (node.val % 2 == 0)
+                    {
+                        return false;
+                    }
+                    if (node.val <= prev)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (node.val % 2 != 0)
+                    {
+                        return false;
+                    }
+                    if (node.val >= prev)
+                    {
+                        return false;
+                    }
+                }
+                prev = node.val;
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+
+            even = !even;
+        }
+
+        return true;
+    }
+}
